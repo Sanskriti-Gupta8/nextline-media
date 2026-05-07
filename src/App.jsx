ff282aa69adbab84199e5e9fe03d23c6664822e9
sanskriti.gupta <sansk85878@gmail.com>
2026-05-07 20:13:12 +0530

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const location = useLocation();
 
+  console.log('hello')
   useEffect(() => {
     window.scrollTo({
       top: 0,
